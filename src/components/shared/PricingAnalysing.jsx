7cce530d18462f761c0f78aa27c5813ef98cc7df
tarek8386 <tmahmud200@gmail.com>
2025-05-08 16:46:12 +0600

    pricing anayliyzing info page completed

--- src/components/shared/PricingAnalysing.jsx
+++ src/components/shared/PricingAnalysing.jsx
@@ -40,7 +40,7 @@ const PricingAnalysing = () => {
           <div className="flex flex-col lg:flex-row justify-between items-center  py-[60px]">
             {/* main form area */}
             <div className="px-7 md:px-[40px] py-[40px] rounded-[48px] bg-[#FFF] w-full sm:max-w-xl md:min-w-[530px] mx-auto">
-              <div className="form-header flex flex-col gap-2  mb-6 lg:mb-12 items-center">
+              <div className="form-header flex flex-col gap-2 items-center">
                 <img
                   src={logo}
                   className="w-[38px] h-[38px] [aspect-ratio:1/1]"
@@ -551,147 +551,105 @@ const PricingAnalysing = () => {
                 {/* Property Information */}
                 <div className="flex flex-col gap-3 md:gap-4 lg:gap-5 py-4 md:py-6 px-2 md-px-4">
                   <PricingTitle titletext="Property Information" />
-                </div>
-
-                <div className="flex flex-col gap-2">
-                  <label className="block text-[#111214] font-[Manrope] text-[15px] md:text-[16px] not-italic font-bold leading-[21.12px] tracking-[-0.16px] mb-1">
-                    Name
-                  </label>
-                  <Controller
-                    name="name"
-                    control={control}
-                    rules={{ required: 'Name is required' }}
-                    render={({ field }) => (
-                      <Input
-                        {...field}
-                        prefix={
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                          >
-                            <path
-                              d="M12.1586 10.87C12.0586 10.86 11.9386 10.86 11.8286 10.87C9.44859 10.79 7.55859 8.84 7.55859 6.44C7.55859 3.99 9.53859 2 11.9986 2C14.4486 2 16.4386 3.99 16.4386 6.44C16.4286 8.84 14.5386 10.79 12.1586 10.87Z"
-                              stroke="#292D32"
-                              strokeWidth="1.5"
-                              strokeLinejoin="round"
-                              strokeLinecap="round"
-                            />
-                            <path
-                              d="M7.15875 14.56C4.73875 16.18 4.73875 18.82 7.15875 20.43C9.90875 22.27 14.4188 22.27 17.1688 20.43C19.5888 18.81 19.5888 16.17 17.1688 14.56C14.4288 12.73 9.91875 12.73 7.15875 14.56Z"
-                              stroke="#292D32"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>
-                        }
-                        placeholder="Robert Lewis"
-                        className="w-full px-4 py-2 border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#09B5FF] bg-[#F3F3F4]"
+                  <div className="flex flex-col gap-3 md:gap-4 lg:gap-5">
+                    {/* property Name */}
+                    <div className="w-full">
+                      <label className="block text-[#111214] font-[Manrope] text-[15px] md:text-[16px] not-italic font-bold leading-[21.12px] tracking-[-0.16px] mb-1">
+                        Property Name
+                        <span className="text-red-500">*</span>
+                      </label>
+                      <Controller
+                        name="propertyName"
+                        control={control}
+                        rules={{ required: 'Property Name is required' }}
+                        render={({ field }) => (
+                          <Input
+                            {...field}
+                            prefix={<></>}
+                            placeholder="Enter property name"
+                            className="w-full px-4 py-2 border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#09B5FF] bg-[#F3F3F4] "
+                          />
+                        )}
                       />
-                    )}
-                  />
 
-                  {errors.name && (
-                    <p className="text-red-500"> {errors.name.message} </p>
-                  )}
-                </div>
-
-                <div className="flex flex-col gap-2">
-                  <label className="block text-[#111214] font-[Manrope] text-[15px] md:text-[16px] not-italic font-bold leading-[21.12px] tracking-[-0.16px] mb-1">
-                    Phone Number
-                  </label>
-                  <Controller
-                    name="phone"
-                    control={control}
-                    rules={{ required: 'Phone number is required' }}
-                    render={({ field }) => (
-                      <PhoneInput
-                        {...field}
-                        country={'us'} // Default country
-                        enableSearch={true}
-                        inputClass="!pl-12 !py-2 !px-4 !bg-[#F3F3F4] !border !border-transparent !rounded-md !w-full border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-[#09B5FF] bg-[#F3F3F4] !h-11 "
-                        buttonClass="!border-none !bg-transparent !left-3"
-                        containerClass="!w-full"
-                        onChange={(value) => {
-                          field.onChange(value);
-                        }}
+                      {errors.propertyName && (
+                        <p className="text-red-500">
+                          {' '}
+                          {errors.propertyName.message}{' '}
+                        </p>
+                      )}
+                    </div>
+                    {/* Property Type* */}
+                    <div className="w-full">
+                      <label className="block text-[#111214] font-[Manrope] text-[15px] md:text-[16px] not-italic font-bold leading-[21.12px] tracking-[-0.16px] mb-1">
+                        Property Type
+                        <span className="text-red-500">*</span>
+                      </label>
+                      <Controller
+                        name="propertyType"
+                        control={control}
+                        rules={{ required: 'Property Type is required' }}
+                        render={({ field }) => (
+                          <Input
+                            {...field}
+                            prefix={<></>}
+                            placeholder="Enter property name"
+                            className="w-full px-4 py-2 border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#09B5FF] bg-[#F3F3F4] "
+                          />
+                        )}
                       />
-                    )}
-                  />
-                  {errors.phone && (
-                    <p className="text-red-500 ">{errors.phone.message}</p>
-                  )}
+
+                      {errors.propertyType && (
+                        <p className="text-red-500">
+                          {' '}
+                          {errors.propertyType.message}{' '}
+                        </p>
+                      )}
+                    </div>
+                    {/* Accessibility Info */}
+                    <div className="w-full">
+                      <label className="block text-[#111214] font-[Manrope] text-[15px] md:text-[16px] not-italic font-bold leading-[21.12px] tracking-[-0.16px] mb-1">
+                        Accessibility Info
+                        <span className="text-red-500">*</span>
+                      </label>
+                      <Controller
+                        name="accessibilityInfo"
+                        control={control}
+                        rules={{ required: 'Accessibility Info is required' }}
+                        render={({ field }) => (
+                          <Input
+                            {...field}
+                            prefix={<></>}
+                            placeholder="Enter property name"
+                            className="w-full px-4 py-2 border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#09B5FF] bg-[#F3F3F4] "
+                          />
+                        )}
+                      />
+
+                      {errors.accessibilityInfo && (
+                        <p className="text-red-500">
+                          {' '}
+                          {errors.accessibilityInfo.message}{' '}
+                        </p>
+                      )}
+                    </div>
+                  </div>
                 </div>
 
-                <div className="flex flex-col gap-2">
-                  <label className="block text-[#111214] font-[Manrope] text-[15px] md:text-[16px] not-italic font-bold leading-[21.12px] tracking-[-0.16px] mb-1">
-                    Gender
-                  </label>
-                  <Controller
-                    name="gender"
-                    control={control}
-                    rules={{ required: 'Gender is required' }}
-                    render={({ field }) => (
-                      <Select
-                        {...field}
-                        placeholder="-- Select Gender --"
-                        allowClear
-                        prefix={
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                          >
-                            <path
-                              d="M12.1586 10.87C12.0586 10.86 11.9386 10.86 11.8286 10.87C9.44859 10.79 7.55859 8.84 7.55859 6.44C7.55859 3.99 9.53859 2 11.9986 2C14.4486 2 16.4386 3.99 16.4386 6.44C16.4286 8.84 14.5386 10.79 12.1586 10.87Z"
-                              stroke="#292D32"
-                              strokeWidth="1.5"
-                              strokeLinejoin="round"
-                              strokeLinecap="round"
-                            />
-                            <path
-                              d="M7.15875 14.56C4.73875 16.18 4.73875 18.82 7.15875 20.43C9.90875 22.27 14.4188 22.27 17.1688 20.43C19.5888 18.81 19.5888 16.17 17.1688 14.56C14.4288 12.73 9.91875 12.73 7.15875 14.56Z"
-                              stroke="#292D32"
-                              strokeWidth="1.5"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>
-                        }
-                        className=""
-                      >
-                        <Option value="male">Male</Option>
-                        <Option value="female">Female</Option>
-                        <Option value="other">Other</Option>
-                      </Select>
-                    )}
-                  />
-                  {errors.gender && (
-                    <p className="text-red-500">{errors.gender.message}</p>
-                  )}
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full bg-[#0A0A0A] py-2 px-4 md:py-3 md:px-6 lg:py-4 lg:px-10 rounded-[16px] hover:bg-[#F0F5F6] hover:text-[#0A0A0A] border border-[#0A0A0A] transition text-[#F0F5F6] font-[Urbanist] text-[16px] not-italic font-medium leading-[25.6px] mt-6 md:mt-8 lg:mt-10"
-                >
-                  Continue
-                </button>
-
-                <p className="text-[#3B3B3B] font-[Urbanist] text-[16px] not-italic font-normal leading-[170%] mx-auto">
-                  Already have an account? {''}
-                  <Link
-                    to="/sign-in"
-                    className="text-[#0A0A0A] font-[Urbanist] text-[16px] not-italic font-semibold leading-[170%] [text-decoration-line:underline] [text-decoration-style:solid] [text-decoration-skip-ink:none] [text-underline-offset:auto] [text-underline-position:from-font]"
+                <div className="btn-wrapper mt-6 md:mt-8 lg:mt-10 flex flex-col md:flex-row gap-2 md:gap-3">
+                  <button
+                    type="submit"
+                    className="w-full bg-[#EAEAEA] py-2 px-4 md:py-3 md:px-6 lg:py-4 lg:px-10 rounded-[16px] hover:bg-[#ee3a3a] hover:text-[#F0F5F6] border border-[#EAEAEA] transition text-[#0A0A0A] font-[Urbanist] text-[16px] not-italic font-medium leading-[25.6px] "
                   >
-                    Sign-in here!
-                  </Link>
-                </p>
+                    Back
+                  </button>
+                  <button
+                    type="submit"
+                    className="w-full bg-[#0A0A0A] py-2 px-4 md:py-3 md:px-6 lg:py-4 lg:px-10 rounded-[16px] hover:bg-[#F0F5F6] hover:text-[#0A0A0A] border border-[#0A0A0A] transition text-[#F0F5F6] font-[Urbanist] text-[16px] not-italic font-medium leading-[25.6px]"
+                  >
+                    Save & Analysis
+                  </button>
+                </div>
               </form>
             </div>
           </div>
