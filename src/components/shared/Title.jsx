6208dcd90bd2d31f404c4a188880d7b60148ca9f
tarek8386 <tmahmud200@gmail.com>
2025-05-18 15:20:48 +0600

    aos added

--- src/components/shared/Title.jsx
+++ src/components/shared/Title.jsx
@@ -5,6 +5,8 @@ const Title = ({ title, className = '' }) => {
   return (
     <>
       <h2
+        data-aos="fade-up"
+        data-aos-duration="1500"
         className={cn(
           'text-[#0E0F11] text-center font-[Manrope] text-[22px] sm:text-[24px] md:text-[32px] xl:text-[48px] not-italic font-bold leading-[33.36px] sm:leading-[43.36px] md:leading-[53.36px] lg:leading-[63.36px] tracking-[-0.48px]',
           className
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import { RouterProvider } from "react-router";
 import router from "./routes/router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+  duration: 1000, // animation duration
+  once: true, // whether animation should happen only once
+});
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
