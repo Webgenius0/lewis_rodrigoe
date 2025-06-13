bbf1ed4b996ef1d4878503dede53b6d1fc276905
Webgenius0 <fullstackdev.softvence@gmail.com>
2025-06-13 19:59:26 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Set alias for "@/"
+      "@": path.resolve(__dirname, "src"),
     },
   },
   preview: {
